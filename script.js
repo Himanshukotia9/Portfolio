599b4062ea64c6733ac904928658e2218f5bb8f9
HimanshuKotia9 <himanshukotia9@gmail.com>
2023-06-10 22:56:31 +0530

    responsiveness completed and animations till about

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ window.onscroll = () =>{
                 links.classList.remove('active')
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // for animation that repeats on scroll use this
+        else{
+            sec.classList.remove('show-animate');
         }
     })
 
